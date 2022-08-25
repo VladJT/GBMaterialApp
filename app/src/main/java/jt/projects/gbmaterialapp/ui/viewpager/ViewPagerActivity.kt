25b2c8e1c8638889b8ad3d9a7bc39204a9220305
Богданков Владислав <onlyrus@hotmail.com>
2022-08-25 20:29:44 +0300

    add CircleIndicator

--- app/src/main/java/jt/projects/gbmaterialapp/ui/viewpager/ViewPagerActivity.kt
+++ app/src/main/java/jt/projects/gbmaterialapp/ui/viewpager/ViewPagerActivity.kt
@@ -1,9 +1,14 @@
 package jt.projects.gbmaterialapp.ui.viewpager
 
+import android.R
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.widget.ViewPager2
 import jt.projects.gbmaterialapp.databinding.ActivityViewPagerBinding
 import jt.projects.gbmaterialapp.viewmodel.ViewPagerAdapter
+import me.relex.circleindicator.CircleIndicator
+
 
 class ViewPagerActivity : AppCompatActivity() {
 
@@ -16,7 +21,10 @@ class ViewPagerActivity : AppCompatActivity() {
         //В activity нам достаточно найти ViewPager в макете, создать адаптер и
         //передать в него SupportFragmentManager. Или ChildFragmentManager, если вы создаёте экраны
         //внутри другого фрагмента
-        binding.viewPager.adapter = ViewPagerAdapter(supportFragmentManager)
+        binding.viewPager.also {
+            it.adapter = ViewPagerAdapter(supportFragmentManager)
+        }
+        binding.indicator.setViewPager(binding.viewPager)
     }
 
 }
